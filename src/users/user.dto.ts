ea5e66541752cfb5d0dcfb798ba648207d55ff09
xchen <7362469@qq.com>
2022-04-13 23:14:10 +0800

    add dto

---
+++ src/users/user.dto.ts
@@ -0,0 +1,6 @@
+import { gender } from '../constant/constant';
+export class UserDto {
+  readonly name: string;
+  readonly age: number;
+  readonly sex: gender;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,25 @@
-import { Controller, Get, Next, Post, Put, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HostParam,
+  Ip,
+  Next,
+  Post,
+  Put,
+  Res,
+  Session,
+} from '@nestjs/common';
 import { UserService } from './users.service';
 import type { Users } from './users';
-
-@Controller('users')
+import type { Response } from 'express';
+import { UserDto } from './user.dto';
+/**
+ * host: 根据请求的host拦截
+ */
+@Controller({ host: '127.0.0.1', path: 'users' })
 export class UsersController {
-  constructor(private readonly appService: UserService) { }
+  constructor(private readonly appService: UserService) {}
 
   /**
    * 使用 @Get() @post方法时 如果返回的是对象或者是一个数组 将会被自动序列化 如果返回的是一个基本类型 将不会做任何处理 只是返回
@@ -24,7 +39,10 @@ export class UsersController {
    * passthrough: true 设置了这个之后 请求之后会将控制权重新交给nest（nest的return就是返回值） 不会影响后续的请求队列 如果没有设置 则执行的权利将还是res的（return无效）
    */
   @Put()
-  putPrice(@Res({ passthrough: true }) res: any, @Next() next): string | void {
+  putPrice(
+    @Res({ passthrough: true }) res: Response,
+    @Next() next,
+  ): string | void {
     // 设置请求头
     res.header('Content-Type', 'text/html');
     // next 走下一个请求的队列
@@ -35,10 +53,34 @@ export class UsersController {
   }
 
   @Put()
-  putPrice1(@Res() res): string {
+  putPrice1(@Res() res: Response): string {
     // res.send({ data: 'foo' });
     console.log('运行了');
     // 由于没有设置 { passthrough: true } 导致return的值无效
     return 'hello world';
+  }
+
+  //TODO: 暂时未知session作用
+  @Post('/session')
+  testSession(@Session() session) {
+    console.log(session);
+  }
+
+  @Post('/ip')
+  testIp(@Ip() ip) {
+    console.log('ip', ip);
+    return ip;
+  }
+
+  @Post('/host')
+  testHost(@HostParam() host) {
+    console.log('host', host);
+    return host;
+  }
+  // TODO: 待完成 自定义装饰器 UserDto 定义接收到的body带有的字段
+  @Post('useDto')
+  useDto(@Body() userDto: UserDto) {
+    console.log(userDto.name);
+    return 'ok';
   }
 }
